dad0c9fa1d1a7b2d30198aec0f5af54c14ae65e8
gjdim <gjdimpter@gmail.com>
2023-05-07 12:15:22 -0400

    Update and merge command classes

--- src/main/java/com/github/mittenmc/serverutils/PermissionCommand.java
+++ src/main/java/com/github/mittenmc/serverutils/PermissionCommand.java
@@ -1,12 +1,16 @@
 package com.github.mittenmc.serverutils;
 
+import org.bukkit.command.CommandSender;
+
 /**
  * Command interface which requires the command to have a permission
  * @author GavvyDizzle
  * @version 1.0.1
  * @since 1.0.1
+ * @deprecated As of release 1.0.3, permissions were merged into the SubCommand class.
  */
 public interface PermissionCommand {
 
+    @Deprecated
     abstract String getPermission();
 }
--- src/main/java/com/github/mittenmc/serverutils/SubCommand.java
+++ src/main/java/com/github/mittenmc/serverutils/SubCommand.java
@@ -8,34 +8,101 @@ import java.util.List;
 /**
  * Implementation for a SubCommand which represents a command unique in its first argument.
  * @author GavvyDizzle
- * @version 1.0.2
+ * @version 1.0.3
  * @since 1.0
  */
 public abstract class SubCommand implements Comparable<SubCommand> {
+
+    private String name, description, syntax, coloredSyntax, permission;
 
     /**
      * @return The name of the subcommand
      * @since 1.0
      */
-    public abstract String getName();
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Change the name of this command
+     * @param name The new name
+     * @since 1.0.3
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
 
     /**
      * @return A short explanation of what this command does
      * @since 1.0
      */
-    public abstract String getDescription();
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * Change the description of this command
+     * @param description The new description
+     * @since 1.0.3
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
     /**
      * @return The command usage
      * @since 1.0
      */
-    public abstract String getSyntax();
+    public String getSyntax() {
+        return syntax;
+    }
+
+    /**
+     * Change the syntax of this command
+     * @param syntax The new syntax
+     * @since 1.0.3
+     */
+    public void setSyntax(String syntax) {
+        this.syntax = syntax;
+    }
 
     /**
      * @return The command usage with colors
      * @since 1.0
      */
-    public abstract String getColoredSyntax();
+    public String getColoredSyntax() {
+        return coloredSyntax;
+    }
+
+    /**
+     * Change the colored syntax of this command
+     * @param coloredSyntax The new colored syntax
+     * @since 1.0.3
+     */
+    public void setColoredSyntax(String coloredSyntax) {
+        this.coloredSyntax = coloredSyntax;
+    }
+
+    /**
+     * Checks this command's permission against the command sender.
+     * If the permission is null, this will return true.
+     * @param commandSender Who sent the command
+     * @return If the sender has permission to use this command
+     * @since 1.0.3
+     */
+    public boolean hasPermission(CommandSender commandSender) {
+        if (permission == null) return true;
+        return commandSender.hasPermission(permission);
+    }
+
+    /**
+     * Change the permission of this command
+     * @param permission The new permission
+     * @since 1.0.3
+     */
+    public void setPermission(String permission) {
+        this.permission = permission;
+    }
 
     /**
      * Called when an individual subcommand is run
@@ -72,6 +139,7 @@ public abstract class SubCommand implements Comparable<SubCommand> {
     public abstract List<String> getSubcommandArguments(CommandSender sender, String[] args);
 
     /**
+     * Sorts commands alphabetically by their name
      *
      * @param o the object to be compared.
      * @return The value of compareTo() given the command name
