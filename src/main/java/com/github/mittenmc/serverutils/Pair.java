ac52842e00429822d18350dd6e1e10e9b1dae2c0
gjdim <gjdimpter@gmail.com>
2023-07-15 11:15:52 -0400

    Pair class

---
+++ src/main/java/com/github/mittenmc/serverutils/Pair.java
@@ -0,0 +1,10 @@
+package com.github.mittenmc.serverutils;
+
+/**
+ * A generic Pair class
+ * @author GavvyDizzle
+ * @version 1.0.4
+ * @since 1.0.4
+ */
+public record Pair<A, B>(A first, B second) {
+}
